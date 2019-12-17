a3e48752036aacfca9d66595bbcff32ac8322ad9
Dave Gaeddert <dave.gaeddert@gmail.com>
2019-12-16 22:14:34 -0600

    Add tag_filter setting (#9)

--- remotes.go
+++ remotes.go
@@ -17,12 +17,20 @@ type remote struct {
 }
 
 type replaceInFile struct {
-	Filename    string `json:"filename"`
-	Pattern     string `json:"pattern"`
-	TagPrefix   string `json:"tag_prefix"`
-	Semver      *bool  `json:"semver"`
-	Prereleases bool   `json:"prereleases"`
-	Range       string `json:"range"`
+	Filename    string     `json:"filename"`
+	Pattern     string     `json:"pattern"`
+	TagPrefix   string     `json:"tag_prefix"` // deprecated
+	TagFilter   *tagFilter `json:"tag_filter"`
+	Semver      *bool      `json:"semver"`
+	Prereleases bool       `json:"prereleases"`
+	Range       string     `json:"range"`
+}
+
+type Tag struct {
+	original string
+	outputAs string
+	sortAs   string
+	semver   *semver.Version
 }
 
 func loadRemotesFromEnv() map[string]*remote {
@@ -57,22 +65,70 @@ func (rif *replaceInFile) writeFile(contents string) {
 	}
 }
 
-func (rif *replaceInFile) getLatestTag(tags []string) string {
-	if rif.TagPrefix != "" {
-		fmt.Printf("Filtering to tags with prefix %s and removing it", rif.TagPrefix)
-		tags = filterAndRemovePrefixes(tags, rif.TagPrefix)
-		fmt.Printf("Remaining tags: %v\n", tags)
+func (rif *replaceInFile) getLatestTag(tagStrs []string) string {
+
+	if rif.TagPrefix != "" && rif.TagFilter != nil {
+		panic("Cannot use tag_prefix and tag_filter together. Please choose one.")
 	}
 
-	// Enabled if not set
-	if rif.Semver == nil || *rif.Semver {
-		versions := stringsToVersions(tags, rif.Range, rif.Prereleases)
-		if len(versions) < 1 && rif.TagPrefix == "" {
-			// Try automatically removing "v" since it's so common
-			tags = filterAndRemovePrefixes(tags, "v")
-			versions = stringsToVersions(tags, rif.Range, rif.Prereleases)
+	if rif.TagPrefix != "" {
+		fmt.Println("DEPRECATED: tag_prefix will be removed in version 1.0")
+	}
+
+	tags := []*Tag{}
+
+	for _, t := range tagStrs {
+		tag := &Tag{
+			original: t,
+			outputAs: t,
+			sortAs:   t,
 		}
-		tags = versionsToStrings(versions)
+
+		if rif.TagPrefix != "" {
+			rif.TagFilter = &tagFilter{
+				Matching: "^" + rif.TagPrefix + "(.*)",
+				OutputAs: "$1",
+				SortAs:   "$1",
+			}
+		}
+
+		if rif.TagFilter != nil {
+			rif.TagFilter.processTag(tag)
+			fmt.Printf("Tag p: %s    output as: %s     sort as: %s\n", tag.original, tag.outputAs, tag.sortAs)
+			if tag.outputAs == "" || tag.sortAs == "" {
+				// Disregard this tag if we didn't get replacements
+				continue
+			}
+		}
+
+		if rif.Semver == nil || *rif.Semver {
+			tag.semver = stringToSemver(tag.sortAs, rif.Range, rif.Prereleases)
+
+			// A "v" prefix is so common that we'll try it automatically
+			if tag.semver == nil && strings.HasPrefix(tag.sortAs, "v") {
+				tag.semver = stringToSemver(tag.sortAs[1:], rif.Range, rif.Prereleases)
+				if tag.semver != nil {
+					// We'll remove the v from the output as well
+					// as a part of this automatic feature
+					tag.outputAs = tag.sortAs[1:]
+				}
+			}
+
+			if tag.semver == nil {
+				// This version was not compatible semver
+				continue
+			}
+		}
+
+		fmt.Printf("Tag original: %s    output as: %s     sort as: %s\n", tag.original, tag.outputAs, tag.sortAs)
+		tags = append(tags, tag)
+	}
+
+	// only sorting we do right now is semver-based
+	if rif.Semver == nil || *rif.Semver {
+		sort.SliceStable(tags, func(i, j int) bool {
+			return tags[i].semver.LT(*tags[j].semver)
+		})
 	}
 
 	if len(tags) < 1 {
@@ -80,50 +136,29 @@ func (rif *replaceInFile) getLatestTag(tags []string) string {
 	}
 
 	latestVersion := tags[len(tags)-1]
-	return latestVersion
+	return latestVersion.outputAs
 }
 
-func stringsToVersions(strs []string, rangeStr string, includePrereleases bool) semver.Versions {
+func stringToSemver(s string, rangeStr string, includePrereleases bool) *semver.Version {
+	version, err := semver.Make(s)
+	if err != nil {
+		// Not a valid semver
+		return nil
+	}
+
+	if len(version.Pre) > 0 && !includePrereleases {
+		// This is a pre-release and they aren't included
+		return nil
+	}
+
 	var semverRange semver.Range
 	if rangeStr != "" {
 		semverRange = semver.MustParseRange(rangeStr)
 	}
-
-	versions := semver.Versions{}
-	for _, s := range strs {
-		version, err := semver.Make(s)
-		if err != nil {
-			// Not a valid semver
-			continue
-		}
-		if len(version.Pre) > 0 && !includePrereleases {
-			// This is a pre-release and they aren't included
-			continue
-		}
-		if semverRange != nil && !semverRange(version) {
-			// There's a range and it's not in range
-			continue
-		}
-		versions = append(versions, version)
+	if semverRange != nil && !semverRange(version) {
+		// There's a range and it's not in range
+		return nil
 	}
-	sort.Sort(versions)
-	return versions
-}
 
-func versionsToStrings(versions semver.Versions) []string {
-	strs := []string{}
-	for _, v := range versions {
-		strs = append(strs, v.String())
-	}
-	return strs
-}
-
-func filterAndRemovePrefixes(tags []string, prefix string) []string {
-	filtered := []string{}
-	for _, s := range tags {
-		if strings.HasPrefix(s, prefix) {
-			filtered = append(filtered, s[len(prefix):])
-		}
-	}
-	return filtered
+	return &version
 }
